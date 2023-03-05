bdda89852adca880cb105139fd990213c134055e
Alberto Valenzuela <avalen28@gmail.com>
2023-03-05 13:27:38 +0100

    iteration 14 done.

--- src/components/FaceBook.jsx
+++ src/components/FaceBook.jsx
@@ -1,11 +1,51 @@
 import profiles from '../data/berlin.json';
+import { useState } from 'react';
 
 const FaceBook = () => {
+  // primera forma de quitar duplicados muy fancy
+  const countryArr = [...new Set(profiles.map((elem) => elem.country))];
+
+  //segunda forma de quitar duplicados.
+  // const countries = profiles.map((elem) => elem.country);
+  // const unique = [...countries].filter(
+  //   (elem, i) => countries.indexOf(elem) === i
+  // );
+
+  //tercera forma. No tan elegante pero same
+  // let countryFiltered = [];
+  // profiles
+  //   .map((elem) => elem.country)
+  //   .forEach((elem) => {
+  //     if (!countryFiltered.includes(elem)) {
+  //       countryFiltered.push(elem);
+  //     }
+  //   });
+
+  const [countrySelected, setCountrySelected] = useState('');
+
+  const handleCountry = (country) => {
+    setCountrySelected(country);
+  };
+
   return (
     <div className="container">
-      {profiles.map((elem) => {
+      {countryArr.map((country, i) => {
+        const color = countrySelected === country ? 'lightblue' : 'white';
         return (
-          <div className="card">
+          <button
+            style={{ backgroundColor: color }}
+            key={i}
+            onClick={() => handleCountry(country)}
+          >
+            {country}
+          </button>
+        );
+      })}
+
+      {profiles.map((elem, i) => {
+        const color = countrySelected === elem.country ? 'lightblue' : 'white';
+        return (
+          <div className="card" key={i} style={{ backgroundColor: color }}>
             <img
               src={elem.img}
               alt="profile"
